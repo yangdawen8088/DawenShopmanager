50ad974f20fdbe2cee7e947d6873e49d2efa322d
Wen Yang <1352559801@qq.com>
2021-10-14 01:53:58 +0800

    init push

---
+++ DawenShopmanager/Form_Login.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace DawenShopmanager
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.SuspendLayout();
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.Location = new System.Drawing.Point(281, 170);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Size = new System.Drawing.Size(100, 35);
+            this.uiButton1.TabIndex = 0;
+            this.uiButton1.Text = "uiButton1";
+            // 
+            // Form_Login
+            // 
+            this.AllowShowTitle = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.uiButton1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(750, 450);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(750, 450);
+            this.Movable = false;
+            this.Name = "Form_Login";
+            this.Padding = new System.Windows.Forms.Padding(0);
+            this.ShowRadius = false;
+            this.ShowRect = false;
+            this.ShowShadow = true;
+            this.ShowTitle = false;
+            this.Text = "用户登录";
+            this.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UIButton uiButton1;
+    }
+}
---
+++ DawenShopmanager/Form_Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace DawenShopmanager
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiAvatar1 = new Sunny.UI.UIAvatar();
+            this.uiLogo1 = new Sunny.UI.UILogo();
+            this.Header.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Footer
+            // 
+            this.Footer.Location = new System.Drawing.Point(252, 662);
+            this.Footer.Size = new System.Drawing.Size(770, 56);
+            this.Footer.Style = Sunny.UI.UIStyle.Custom;
+            // 
+            // Aside
+            // 
+            this.Aside.Location = new System.Drawing.Point(2, 145);
+            this.Aside.SelectedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Aside.SelectedHighColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Aside.Size = new System.Drawing.Size(250, 573);
+            this.Aside.Style = Sunny.UI.UIStyle.Custom;
+            // 
+            // Header
+            // 
+            this.Header.Controls.Add(this.uiLogo1);
+            this.Header.Controls.Add(this.uiAvatar1);
+            this.Header.Location = new System.Drawing.Point(2, 35);
+            this.Header.MenuStyle = Sunny.UI.UIMenuStyle.Custom;
+            this.Header.SelectedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Header.SelectedHighColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Header.Size = new System.Drawing.Size(1020, 110);
+            this.Header.Style = Sunny.UI.UIStyle.Custom;
+            // 
+            // uiAvatar1
+            // 
+            this.uiAvatar1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar1.Location = new System.Drawing.Point(957, 24);
+            this.uiAvatar1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar1.Name = "uiAvatar1";
+            this.uiAvatar1.Size = new System.Drawing.Size(60, 60);
+            this.uiAvatar1.TabIndex = 0;
+            this.uiAvatar1.Text = "uiAvatar1";
+            // 
+            // uiLogo1
+            // 
+            this.uiLogo1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiLogo1.Location = new System.Drawing.Point(20, 24);
+            this.uiLogo1.MaximumSize = new System.Drawing.Size(300, 80);
+            this.uiLogo1.MinimumSize = new System.Drawing.Size(300, 80);
+            this.uiLogo1.Name = "uiLogo1";
+            this.uiLogo1.Size = new System.Drawing.Size(300, 80);
+            this.uiLogo1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLogo1.TabIndex = 4;
+            this.uiLogo1.Text = "uiLogo1";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1024, 720);
+            this.ExtendBox = true;
+            this.MinimumSize = new System.Drawing.Size(1024, 720);
+            this.Name = "Form_Main";
+            this.Padding = new System.Windows.Forms.Padding(2, 35, 2, 2);
+            this.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(222)))), ((int)(((byte)(153)))));
+            this.ShowDragStretch = true;
+            this.ShowIcon = true;
+            this.ShowRadius = false;
+            this.ShowShadow = true;
+            this.ShowTitleIcon = true;
+            this.Style = Sunny.UI.UIStyle.Custom;
+            this.StyleCustomMode = true;
+            this.Text = "DaWen 店长";
+            this.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(222)))), ((int)(((byte)(153)))));
+            this.TitleFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Header.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UILogo uiLogo1;
+        private Sunny.UI.UIAvatar uiAvatar1;
+    }
+}
+
